4263694b2f4386db7cdd59c7a75c132d0a04408b
Rajan Ladwa <rladwa@gmail.com>
2023-07-19 16:56:36 +0900

    amended admin error

--- groups/admin.py
+++ groups/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+class GroupMemberInline(admin.TabularInline): # allows admin to look at group members through group and edit those too.
+    model = models.GroupMember
+
+admin.site.register(models.Group)
